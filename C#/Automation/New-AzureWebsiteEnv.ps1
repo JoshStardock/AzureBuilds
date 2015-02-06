150181d6d84374e61eaea96891db62fbdfe7562d
JoshStardock <jmclean@stardock.com>
2015-02-06 09:35:42 -0600

    resolving pathing issues

--- C#/Automation/New-AzureWebsiteEnv.ps1
+++ C#/Automation/New-AzureWebsiteEnv.ps1
@@ -7,6 +7,7 @@
     [String]$SqlDatabasePassword,
 	[String]$SubscriptionName,
 	[String]$AppInsightsKey,
+	[String]$CSProjPath,
 	[String]$EnvType
     )
 
@@ -142,7 +143,7 @@ Write-Verbose "[Finish] creating Windows Azure environment: $Name"
 
 # Run MSBuild to publish the project
 
-$ProjectFile = Get-ChildItem %teamcity.build.checkoutDir%| Where-Object {$_.Name -eq "MyFixIT.csproj"} | select FullName
+$ProjectFile = Get-ChildItem $CSProjPath| Where-Object {$_.Name -eq "MyFixIT.csproj"} | select FullName
 Write-Verbose "The value of `$ProjectFile is:  $ProjectFile"
 <#
 & "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $ProjectFile `
