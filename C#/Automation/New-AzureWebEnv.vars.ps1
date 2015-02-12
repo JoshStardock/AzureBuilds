9bd95818404713992c4933bff3453051ebb4c288
JoshStardock <jmclean@stardock.com>
2015-02-12 11:43:48 -0600

    Delete New-AzureWebEnv.vars.ps1

--- C#/Automation/New-AzureWebEnv.vars.ps1
+++
@@ -1,8 +0,0 @@
-. .\New-RandomPass.ps1
-
-$params = @{'name' = 'name';
-'location' = 'location';
-'SqlDatabaseUserName' = 'dbusername';
-'SqlDatabasePassword' = New-RandomPass -MinPasswordLength 12 -MaxPasswordLength 15 -Count 1;
-'slot' = 'staging';
-}
